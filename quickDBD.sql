5477f21ad0fae15c174176976d9ac03def1217d5
sninh <80871405+sninh@users.noreply.github.com>
2021-06-30 21:33:24 -0700

    Add files via upload

--- quickDBD.sql
+++ quickDBD.sql
@@ -1,39 +1,44 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/voTMBM
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
-CREATE TABLE "SPY" (
-    "date_time" date   NOT NULL,
-    "stock_price" varchar   NOT NULL,
-    "ticker" varchar   NOT NULL,
+﻿CREATE TABLE "spy" (
+    "date_time_on_30_min" varchar,
+    "Ticker" varchar,
+    "Open" DECIMAL,
+    "High" DECIMAL,
+    "Low" DECIMAL,
+    "Close" DECIMAL,
+    "Adj_Close" DECIMAL,
+    "Volume" DECIMAL,
+    "Percent_Change_From_Startdate" DECIMAL,
+    "Change_Close_Less_Open" DECIMAL,
     CONSTRAINT "pk_SPY" PRIMARY KEY (
-        "date_time"
+        "date_time_on_30_min"
      )
 );
 
-CREATE TABLE "Title_category" (
-    "url" varchar   NOT NULL,
-    "title" varchar   NOT NULL,
-    "category" varchar   NOT NULL,
-    CONSTRAINT "pk_Title_category" PRIMARY KEY (
+CREATE TABLE "uuq" (
+    "date_time_on_30_min" varchar,
+    "Ticker" varchar,
+    "Open" DECIMAL,
+    "High" DECIMAL,
+    "Low" DECIMAL,
+    "Close" DECIMAL,
+    "Adj_Close" DECIMAL,
+    "Volume" DECIMAL,
+    "Percent_Change_From_Startdate" DECIMAL,
+    "Change_Close_Less_Open" DECIMAL,
+    CONSTRAINT "pk_UUQ" PRIMARY KEY (
+        "date_time_on_30_min"
+     )
+);
+
+CREATE TABLE "press" (
+    "url" varchar,
+    "title" varchar,
+    "category" varchar,
+    "date" date,
+    "time" varchar,
+    "date_time_on_30_min" varchar,
+    "text" varchar,
+    CONSTRAINT "pk_Press" PRIMARY KEY (
         "url"
      )
 );
-
-CREATE TABLE "date_time" (
-    "url" varchar   NOT NULL,
-    "date" date   NOT NULL,
-    "time" varchar   NOT NULL,
-    "date_time" date   NOT NULL,
-    CONSTRAINT "pk_date_time" PRIMARY KEY (
-        "url"
-     )
-);
-
-ALTER TABLE "Title_category" ADD CONSTRAINT "fk_Title_category_url" FOREIGN KEY("url")
-REFERENCES "date_time" ("url");
-
-ALTER TABLE "date_time" ADD CONSTRAINT "fk_date_time_date_time" FOREIGN KEY("date_time")
-REFERENCES "SPY" ("date_time");
-
